9f8fecaee5ce0cb9dec67db0fb9614861c646156
SuperSimonC <65116136+SuperSimonC@users.noreply.github.com>
2020-07-26 21:48:24 +1000

    Create IoT Hub Client

--- myDevice.py
+++ myDevice.py
@@ -7,3 +7,7 @@ from azure.iot.device import IoTHubDeviceClient, Message
 # The device connection string to authenticate the device
 CONNECTION_STRING = "HostName=msa-myhub.azure-devices.net;DeviceId=SimulatedDevice;SharedAccessKey=QY82Cbyfk87YJyYiOwtN9qye1LvusnWQIwI7+Gf9TaQ="
 
+def iothub_client_init():
+    # Create an IoT Hub client
+    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+    return client
